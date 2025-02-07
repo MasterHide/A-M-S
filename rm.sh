7beff42fdac84f020305723eb1e9d84405b18bca
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-02-07 10:29:34 +0530

    rm.sh

--- rm.sh
+++ rm.sh
@@ -6,5 +6,8 @@ rm -f /root/sr-system.sh
 rm -f /home/ubuntu/sr-system.sh
 rm -f /opt/sr-system.sh
 rm -f /opt/hiddify-manager/sr-system.sh
-
+rm -f /root/rm.sh
+rm -f /home/ubuntu/rm.sh
+rm -f /opt/rm.sh
+rm -f /opt/hiddify-manager/rm.sh
 echo "Removed the sr-system.sh file from /root, /home/ubuntu, and /opt."
