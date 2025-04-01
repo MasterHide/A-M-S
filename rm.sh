43724ff789eef451e45c5d79c8d3fbcb12edc31d
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-04-01 18:08:20 +0530

    Update rm.sh

--- rm.sh
+++ rm.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Remove the cron job related to reboot
 echo "Removing any existing cron jobs related to reboot..."
 (crontab -l 2>/dev/null | grep -v "reboot") | crontab -
@@ -6,20 +8,20 @@ echo "Removed any existing cron jobs related to reboot."
 # Remove specific files from multiple paths
 echo "Cleaning up old files..."
 
-# Define the list of files to remove
-files_to_remove=(
-    "/root/sr-system.sh"
-    "/home/ubuntu/sr-system.sh"
-    "/opt/sr-system.sh"
-    "/opt/hiddify-manager/sr-system.sh"
-    "/root/rm.sh"
-    "/home/ubuntu/rm.sh"
-    "/opt/rm.sh"
-    "/opt/hiddify-manager/rm.sh"
-)
+# List of files to remove
+files_to_remove="
+/root/sr-system.sh
+/home/ubuntu/sr-system.sh
+/opt/sr-system.sh
+/opt/hiddify-manager/sr-system.sh
+/root/rm.sh
+/home/ubuntu/rm.sh
+/opt/rm.sh
+/opt/hiddify-manager/rm.sh
+"
 
-# Loop through the list and remove each file if it exists
-for file in "${files_to_remove[@]}"; do
+# Loop through each file and remove it if it exists
+for file in $files_to_remove; do
     if [ -f "$file" ]; then
         sudo rm -f "$file"
         echo "Removed: $file"
